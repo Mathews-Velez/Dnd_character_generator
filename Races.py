73ce203f6f7ddedcd47fb13d9aedaefcc79e5efd
Matt <velezmathew@gmail.com>
2020-05-05 15:47:37 -0400

    Addition of comments for ease of understanding

--- Races.py
+++ Races.py
@@ -1,4 +1,5 @@
 #import all the files from Race_files
 from Race_files import Aasimar_Race, Dragonborn_Race, Dwarf_Race, Elf_Race, Goliath_Race, Half_Orc_Race, Human_Race, Tiefling_Race,Kenku_Race
-#Assign race strings to race files
+
+#Assign race strings in dictionary to race files
 race_list = {'Aasimar': Aasimar_Race.Aasimar, 'Dragonborn':Dragonborn_Race.Dragonborn, 'Dwarf':Dwarf_Race.Dwarf, 'Elf':Elf_Race.Elf, 'Goliath':Goliath_Race.Goliath, 'Half_Orc':Half_Orc_Race.Half_Orc, 'Human':Human_Race.Human, 'Tiefling': Tiefling_Race.Tiefling, 'Kenku': Kenku_Race.Kenku}
