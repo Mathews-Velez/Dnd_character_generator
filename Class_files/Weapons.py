4c879a256e47104ea62901209c7fe4318173d449
Matt <velezmathew@gmail.com>
2020-05-25 10:24:09 -0400

    Completion of weapons.py

--- Class_files/Weapons.py
+++ Class_files/Weapons.py
@@ -15,4 +15,12 @@ def simple_ranged_weapon():
 
 #for martial melee weapons 
 def martial_melee_weapon():
-    martial_melee_weapons = {'Battleaxe':'1d8 slashing','Flail':'1d8 bludgeoning','Glaive':'1d10 slashing','Greataxe':'1d12','Greatsword':'2d6','Halberd':'1d10 slashing','Lance':'1d12 piercing','Longsword':'1d8 slashing','Maul':'2d6 bludgeoning'}
+    martial_melee_weapons = {'Battleaxe':'1d8 slashing','Flail':'1d8 bludgeoning','Glaive':'1d10 slashing','Greataxe':'1d12 slashing','Greatsword':'2d6 slashing','Halberd':'1d10 slashing','Lance':'1d12 piercing','Longsword':'1d8 slashing','Maul':'2d6 bludgeoning', 'Morning star':'1d8 piercing', 'Pike':'1d10 piercing','Rapier':'1d8 piercing','Scimitar':'1d6 slashing','Shortsword':'1d6 piercing','Trident':'1d6 piercing','War pick ':'1d8 piercing','Warhammer':'1d8 bludgeoning','Whip':'1d4 slashing'}
+    weapon, damage = random.choice(list(martial_melee_weapons.items()))
+    return weapon, damage
+
+#for martial ranged weapons
+def martial_ranged_weapon():
+    martial_ranged_weapons = {'Blowgun':'1 piercing','Crossbow, hand':'1d6 piercing','Crossbow, heavy':'1d10','Longbow':'1d8 piercing'}
+    weapon, damage = random.choice(list(martial_ranged_weapons.itmes()))
+    return weapon, damage
