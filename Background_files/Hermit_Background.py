8f6eadb6605860313a37f0865c21522910cc8967
Matt <velezmathew@gmail.com>
2020-05-26 09:50:16 -0400

    Addition of all empty backgrounds

---
+++ Background_files/Hermit_Background.py
@@ -0,0 +1,17 @@
+import random 
+# background story
+def background():
+    #Personality trait
+     Personality_traits = ("","","","","","","","")
+     Personality_trait =random.choice(Personality_traits)
+     #Ideal
+     Ideals = ("","","","","","","","")
+     Ideals =random.choice(Ideals)
+     #Bond
+     Bonds = ("","","","","","","","")
+     Bond =random.choice(Bonds)
+     #Flaw
+     Flaws = ("","","","","","","","")
+     Flaw =random.choice(Flaws)
+     
+     
