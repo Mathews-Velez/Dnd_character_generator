9e8f87433a0db64b562a3979d245a148b22466f1
Sean Christians <seanchristians.scc@gmail.com>
2020-06-14 15:48:21 -0400

    Fix variable names

--- src/main.py
+++ src/main.py
@@ -5,21 +5,21 @@ while True:
 
   print(' Welcome to Dungeon and Dragons Character Generator \n')
 
-  race = random.choice(Races.race_list.keys())
-  print(f'Race: {chosen_race}')
+  race = random.choice(list(Races.race_list.keys()))
+  print(f'Race: {race}')
 
-  class_ = random.choice(Classes.class_list.keys())
+  class_ = random.choice(list(Classes.class_list.keys()))
   print(f'Class: {class_}')
 
-  background = random.choice(Background.background_list.keys())
+  background = random.choice(list(Backgrounds.background_list.keys()))
   print(f'Background: {background}')
 
   gender = random.choice(["Male", "Female"])
   print(f'Gender: {gender}')
 
-  Races.race_list[chosen_race](chosen_gender)# generate all info on the character's race
-  Classes.class_list[chosen_class](chosen_race)# generate all the info on the characters class
-  Backgrounds.background_list[chosen_background]()# generate all the info on the characters background
+  Races.race_list[race](gender)# generate all info on the character's race
+  Classes.class_list[class_](race)# generate all the info on the characters class
+  Backgrounds.background_list[background]()# generate all the info on the characters background
 
   #Exit Function
   try_again = input('\n\nTry again? (y/n): ')
