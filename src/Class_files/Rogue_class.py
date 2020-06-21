e3112dce782e68a479c1bb6659c714a803ed4e63
Mathew-Velez <velezmathew@gmail.com>
2020-06-21 12:53:15 -0400

    rogue class completed

--- src/Class_files/Rogue_class.py
+++ src/Class_files/Rogue_class.py
@@ -4,62 +4,64 @@ import Class_files.Equipment_packs as Equipment_packs
 def Rogue(x):
 
 	#Print breif description
-	print('\n\nYour class\n\nMonk\n -Martial artists who can channel their Ki to do impressive feats as if they were magic users. No armor is necessary, just dexterity and the power of your mind. Deflect missiles thrown at you, changing the target to your enemies, move at the speed of the wind and fill your foes’ faces with a flurry blows.')
-	#Print Class Features
-	print('For stat priority, Dexterity followed by Wisdom and then Constitution')
+	print('\n\nYour class\n\nRogue\n -Let’s get shady, grab a dagger and start stabbing. Rogues excel at sneaking around, scouting ahead, being dexterous and about everything you would expect a thief to be good at. In dungeons, they can help their party by deactivating traps or opening locked doors. Don’t expect them to wear much armor, nor be able to carry heavy stuff; but were your task to need some delicacy or swashbuckling, you’ve found the right person.')
+
+	print('\nFor stat priority, Dexterity followed by Intelligence or Charisma\n')
 
 	#Hit points
-	#Hit Dice:
 	print('Hit Dice\n -d8')
-	#Hit Points at 1st Level:
 	print('Hit points at Level 1\n 8 + Constitution_mod')
-	#Hit Points at Higher Levels: 1d8 (or 5) + your constitution modifier per barbarian level after 1st
-	print('Hit Points at Higher Levels: 1d10(or 6) + your constitution modifier per Fighter level after 1st')
+	print('Hit Points at Higher Levels: 1d8(or 5) + your constitution modifier per Rogue level after 1st')
 
 	#Proficiencies
 	print('\nProficiencies')
-	#Armor:
-	print('-Armour:\n -All armor and shields')
-	#Weapons:
-	print('-Weapons:\n -Simple weapons, Martial weapons')
-	#Tools: None
-	print('-Tools:\n -None')
-	#Saving Throws:
-	print('-Saving Throws:\n -Strength, Constitution')
+	print('-Armour:\n -Light armour')
+	print('-Weapons:\n -Simple weapons,Hand crossbow,Longswords,Rapiers, Shortswords')
+	print("-Tools:\n -Theive's tools")
+	print('-Saving Throws:\n -Dexterity, Intelligence')
 
 	#Skills
-	skills = ('Acrobatics','Animal Handling','Arcana','Athletics','Deception','History','Insight','Intimidation','Investigation','Medicine','Nature','Perception','Performance','Persuassion','Religion','Sleight of Hand','Stealth','Survival')
-	#fetching 2 unique strings from the tuple skills
+	skills = ('Acrobatics','Athletics','Deception','Insight','Intimidation','Investigation','Perception','Performance','Persuassion','Sleight of hand','Stealth')
 	chosen_skills = random.sample(skills,2)
 	print(f'Skills:\n -{chosen_skills}')
 
 	#equipment
 	print('\nClass Equipment')
+
 	#weapons
 	print('-Weapons')
 
 	#first choice weapon
 	first_choice = random.choice([1,2])
 	if first_choice == 1:
-		weapon,damage = Weapons.martial_melee_weapon()
+		weapon,damage = Weapons.martial_melee_weapon('Rapier')
 	elif first_choice == 2:
-		weapon,damage= Weapons.martial_melee_weapon()
-
+		weapon,damage= Weapons.martial_melee_weapon('Shortsword')
 	print(f'  Primary weapon {weapon,damage}')
 
 	#second weapon choice
 	second_choice = random.choice([1,2])
 	if second_choice == 1:
 		weapon, damage = Weapons.simple_ranged_weapon('Light Crossbow')
-	elif second_choice == 2:
-		weapon, damage = Weapons.simple_melee_weapon('Handaxe')
-	print(f'  Secondary weapon{weapon,damage}')
+		print(f'  Secondary weapon{weapon,damage} with 20 arrows')
+	elif second_choice == 2: 
+		weapon, damage = Weapons.martial_melee_weapon('Shortsword')
+		print(f'  Secondary weapon{weapon,damage}')
+	#third weapon choice
+	weapon, damage = Weapons.simple_melee_weapon('Dagger')
+	print(f'  Third weapon 2{weapon,damage}')
+	
+	#armor
+	print('-Armor')
+	print(' Leather armor')
 
 	#equipment pack
 	print('-equipment pack')
-	Thrid_choice = random.choice([1,2])
-	if Thrid_choice == 1:
+	packs = random.choice([1,2,3])
+	if packs == 1:
 		equipment_pack = Equipment_packs.equipment_pack('Dungeoneers_pack')
-	if Thrid_choice == 2:
+	if packs == 2:
 		equipment_pack = Equipment_packs.equipment_pack('Explorers_pack')
+	if packs == 3:
+		equipment_pack = Equipment_packs.equipment_pack('Burglars_pack')
 	print(f'  {equipment_pack}')
