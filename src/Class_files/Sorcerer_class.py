c0eca0b875659fb00011c4d33c51cedfa696415a
Mathew-Velez <velezmathew@gmail.com>
2020-06-26 12:10:37 -0400

    Completion of Sorcerer class

--- src/Class_files/Sorcerer_class.py
+++ src/Class_files/Sorcerer_class.py
@@ -4,62 +4,62 @@ from dnd_character_generator.Class_files import Equipment_packs
 def Sorcerer(x):
 
 	#Print breif description
-	print('\n\nYour class\n\nMonk\n -Martial artists who can channel their Ki to do impressive feats as if they were magic users. No armor is necessary, just dexterity and the power of your mind. Deflect missiles thrown at you, changing the target to your enemies, move at the speed of the wind and fill your foes’ faces with a flurry blows.')
+	print('\n\nYour class\n\nSorcerer\n -Due to some random or pre-established occurrence, magic runs through you. This means you can “cheat” in a certain way to be able to use magic at will, instead of having to learn how to cast it. Sorcerers are also equipped with a pool of magic they get their sorcery points from, letting them twist spells to their will in different ways.')
 	#Print Class Features
-	print('For stat priority, Dexterity followed by Wisdom and then Constitution')
+	print('For stat priority, Charisma followed by Constitution')
 
 	#Hit points
-	#Hit Dice:
-	print('Hit Dice\n -d8')
-	#Hit Points at 1st Level:
-	print('Hit points at Level 1\n 8 + Constitution_mod')
-	#Hit Points at Higher Levels: 1d8 (or 5) + your constitution modifier per barbarian level after 1st
-	print('Hit Points at Higher Levels: 1d10(or 6) + your constitution modifier per Fighter level after 1st')
+	print('Hit Dice\n -d6')
+	print('Hit points at Level 1\n 6 + Constitution_mod')
+	print('Hit Points at Higher Levels: 1d6(or 4) + your constitution modifier per Sorcerer level after 1st')
 
 	#Proficiencies
 	print('\nProficiencies')
-	#Armor:
-	print('-Armour:\n -All armor and shields')
-	#Weapons:
-	print('-Weapons:\n -Simple weapons, Martial weapons')
-	#Tools: None
+	print('-Armour:\n -None')
+	print('-Weapons:\n -Daggers, Darts, Slings, Quarterstaffs, Light crossbow')
 	print('-Tools:\n -None')
-	#Saving Throws:
-	print('-Saving Throws:\n -Strength, Constitution')
+	print('-Saving Throws:\n -Charisma, Constitution')
 
 	#Skills
-	skills = ('Acrobatics','Animal Handling','Arcana','Athletics','Deception','History','Insight','Intimidation','Investigation','Medicine','Nature','Perception','Performance','Persuassion','Religion','Sleight of Hand','Stealth','Survival')
-	#fetching 2 unique strings from the tuple skills
+	skills = ('Arcana','Deception','Intimidation','Persuasion','Religion')
 	chosen_skills = random.sample(skills,2)
 	print(f'Skills:\n -{chosen_skills}')
 
 	#equipment
 	print('\nClass Equipment')
+
 	#weapons
 	print('-Weapons')
 
 	#first choice weapon
 	first_choice = random.choice([1,2])
 	if first_choice == 1:
-		weapon,damage = Weapons.martial_melee_weapon()
+		weapon,damage = Weapons.simple_ranged_weapon('Light Crossbow')
+		print(f'  Primary weapon{weapon,damage} and 20 bolts')
 	elif first_choice == 2:
-		weapon,damage= Weapons.martial_melee_weapon()
-
-	print(f'  Primary weapon {weapon,damage}')
+		r_or_m = random.choice(['r','m'])
+		if r_or_m == 'r':
+			weapon,damage = Weapons.simple_ranged_weapon()
+		elif r_or_m == 'm':
+			weapon,damage = Weapons.simple_melee_weapon()
+		print(f'  Primary weapon {weapon,damage}')
 
 	#second weapon choice
-	second_choice = random.choice([1,2])
-	if second_choice == 1:
-		weapon, damage = Weapons.simple_ranged_weapon('Light Crossbow')
-	elif second_choice == 2:
-		weapon, damage = Weapons.simple_melee_weapon('Handaxe')
-	print(f'  Secondary weapon{weapon,damage}')
+	second_choice = random.choice(['c','a'])
+	if second_choice == 'c':
+		spellcast_helper = 'a component pouch'
+	elif second_choice =='a':
+		spellcast_helper = 'an arcance focus'
+	print(f'  Spellcasting helper: {spellcast_helper}')
 
 	#equipment pack
 	print('-equipment pack')
 	Thrid_choice = random.choice([1,2])
 	if Thrid_choice == 1:
 		equipment_pack = Equipment_packs.equipment_pack('Dungeoneers_pack')
-	if Thrid_choice == 2:
+		print(f'  Dungeoneers pack:{equipment_pack}')
+
+	elif Thrid_choice == 2:
 		equipment_pack = Equipment_packs.equipment_pack('Explorers_pack')
-	print(f'  {equipment_pack}')
+		weapon,damage = Weapons.simple_melee_weapon('Dagger')
+		print(f'  Explorers pack:{equipment_pack} and 2{weapon,damage}')
