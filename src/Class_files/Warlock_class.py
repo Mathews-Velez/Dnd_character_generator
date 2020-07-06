5075a47a31e93962a9cb83d1536c2fc93c1d0b01
Mathew-Velez <velezmathew@gmail.com>
2020-07-06 12:41:21 -0400

    Warlock class done

--- src/Class_files/Warlock_class.py
+++ src/Class_files/Warlock_class.py
@@ -4,31 +4,31 @@ from dnd_character_generator.Class_files import Equipment_packs
 def Warlock(x):
 
 	#Print breif description
-	print('\n\nYour class\n\nWarlock\n -')
+	print('\n\nYour class\n\nWarlock\n -Warlocks make pacts with extremely powerful beings, who don’t mind lending some power as they have so much they don’t even notice. However, it all comes with a price: There’s a reason this entity decided to accept the pact, and you might need to do some stuff to maintain the accord. Decide whether you want to have a big spellbook full of incantations, a strange summonable weapon or weird servants. Will you perish in the way to becoming an eldritch master?')
 	#Print Class Features
-	print('For stat priority, Dexterity followed by Wisdom and then Constitution')
+	print('For stat priority, Charisma followed by Wisdom and then Constitution')
 
 	#Hit points
 	#Hit Dice:
-	print('Hit Dice\n -')
+	print('Hit Dice\n -1d6 per sorcerer level')
 	#Hit Points at 1st Level:
-	print('Hit points at Level 1\n  + Constitution_mod')
+	print('Hit points at Level 1\n  6 + Constitution_mod')
 	#Hit Points at Higher Levels:
-	print('Hit Points at Higher Levels:  + your constitution modifier per Warlock level after 1st')
+	print('Hit Points at Higher Levels: 1d6(or 4)+ your constitution modifier per Warlock level after 1st')
 
 	#Proficiencies
 	print('\nProficiencies')
 	#Armor:
-	print('-Armour:\n -')
+	print('-Armour:\n -None')
 	#Weapons:
-	print('-Weapons:\n -')
+	print('-Weapons:\n -Daggers, Darts, Slings, Quarterstaffs, Light Crossbows')
 	#Tools: None
-	print('-Tools:\n -')
+	print('-Tools:\n -None')
 	#Saving Throws:
-	print('-Saving Throws:\n -')
+	print('-Saving Throws:\n -Constitution, Charisma')
 
 	#Skills
-	skills = ('')
+	skills = ('Arcana','Deception','Insight','Intimidation','Persuassion','Religion','Religion')
 	#fetching 2 unique strings from the tuple skills
 	chosen_skills = random.sample(skills,2)
 	print(f'Skills:\n -{chosen_skills}')
@@ -41,19 +41,23 @@ def Warlock(x):
 	#first choice weapon
 	first_choice = random.choice([1,2])
 	if first_choice == 1:
-		weapon,damage = Weapons.martial_melee_weapon()
+		weapon,damage = Weapons.simple_ranged_weapon('Light Crossbow')
+		print(f'  -Primary weapon: {weapon,damage}')
 	elif first_choice == 2:
-		weapon,damage= Weapons.martial_melee_weapon()
-
-	print(f'  Primary weapon {weapon,damage}')
+		r_or_m = random.choice(['r','m'])
+		if r_or_m == 'r':
+			weapon,damage = Weapons.simple_ranged_weapon()
+		elif r_or_m == 'm':
+			weapon,damage = Weapons.simple_melee_weapon()
+		print(f'  Primary weapon {weapon,damage}')
 
 	#second weapon choice
-	second_choice = random.choice([1,2])
-	if second_choice == 1:
-		weapon, damage = Weapons.simple_ranged_weapon('Light Crossbow')
-	elif second_choice == 2:
-		weapon, damage = Weapons.simple_melee_weapon('Handaxe')
-	print(f'  Secondary weapon{weapon,damage}')
+	second_choice = random.choice(['c','a'])
+	if second_choice == 'c':
+		spellcast_helper = 'a component pouch'
+	elif second_choice =='a':
+		spellcast_helper = 'an arcance focus'
+	print(f'  Spellcasting helper: {spellcast_helper}')
 
 	#equipment pack
 	print('-equipment pack')
