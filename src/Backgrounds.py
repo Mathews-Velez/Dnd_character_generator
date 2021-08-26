aa88171ff6567ff73ed92e4913ab4e0483245bbf
Mathews-Velez <velezmathew@gmail.com>
2021-08-25 21:14:38 -0400

    added entertainer to bacgrounds.py file

--- src/Backgrounds.py
+++ src/Backgrounds.py
@@ -1,15 +1,20 @@
-from dnd_character_generator.Background_files import Acolyte_Background, Charlatan_Background, Criminal_Background
+from dnd_character_generator.Background_files import Acolyte_Background,\
+	 Charlatan_Background,\
+	 Criminal_Background,\
+	 Entertainer_Background
 
 background_list = {
 	'Acolyte': Acolyte_Background.Acolyte,
 	'Charlatan': Charlatan_Background.Charlatan,
-	'Criminal': Criminal_Background.Criminal
+	'Criminal': Criminal_Background.Criminal,
+	'Entertainer': Entertainer_Background.Entertainer,
 }
 
 background_skill_profinciencies = {
 	'Acolyte': ['Insight','Religion'],
 	'Charlatan':['Deception','Sleight of hand'],
-	'Criminal':['Deception','Steallth']
+	'Criminal':['Deception','Steallth'],
+	'Entertainer':['Acrobatics','Performance'],
 }
 
 
